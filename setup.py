6a0fe168d0b82eeb3d4b76f4feae0976339c7e0f
Tom <thomas.d.maguire@btinternet.com>
2023-06-02 15:58:27 +0200

    Changes to repo

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ import sys
 
 try:
     from skbuild import setup
-    import nanobind
     import torch
+    import nanobind
+    
 except ImportError:
     print("The preferred way to invoke 'setup.py' is via pip, as in 'pip "
           "install .'. If you wish to run the setup script directly, you must "
@@ -12,16 +13,16 @@ except ImportError:
     raise
 
 setup(
-    name="altmin",
+    name="fast_altmin",
     version="0.0.1",
-    author="Wenzel Jakob",
-    author_email="wenzel.jakob@epfl.ch",
-    description="An example minimal project that compiles bindings using nanobind and scikit-build",
-    url="https://github.com/wjakob/nanobind_example",
+    author="Tom Maguire",
+    author_email="thomas.d.maguire@btinternet.com",
+    description="cpp implementation of alternating minimisation",
+    url="https://github.com/DiamonDinoia/altmin",
     license="BSD",
-    packages=['altmin'],
+    packages=['fast_altmin'],
     package_dir={'': 'src'},
-    cmake_install_dir="src/altmin",
+    cmake_install_dir="src/fast_altmin",
     include_package_data=True,
-    python_requires=">=3.8"
+    python_requires=">=3.7"
 )
---
+++ src/fast_altmin/__init__.py
@@ -0,0 +1,3 @@
+import torch
+from .fast_altmin import * 
+from .control_flow import *
